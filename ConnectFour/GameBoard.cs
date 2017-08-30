e2defbbea209d684f4aa285f65e544e91ab93451
steven2805 <stevenmac90@googlemail.com>
2017-08-30 11:05:23 +0100

    initial commit

---
+++ ConnectFour/GameBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace ConnectFour
+{
+    public class GameBoard
+    {
+        public ArrayList testing;
+        private int heightGrid;
+        private int widthGrid;
+
+
+        public GameBoard(int height, int width)
+        {
+            testing = GridBuilding();
+            heightGrid = height;
+            widthGrid = width;
+        }
+
+        public int GetLengthGrid(){
+            return heightGrid;
+        }
+
+        private ArrayList GridBuilding(){
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Hello");
+
+
+            return myAL;
+        }
+ 
+
+
+    }
+}
---
+++ ConnectFour/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ConnectFour
+{
+    
+    [Activity(Label = "ConnectFour", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        GameBoard game;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            game = new GameBoard(10,7);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
